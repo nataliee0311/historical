fa974e454628a6492fda4b023eb6a522406a198f
Folaido <feizr424@gmail.com>
2021-04-11 15:12:38 -0500

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import Topbar from "./Topbar";
 import "../css/home.css";
 import { Footer as Bottom } from "./Footer";
 import { Layout, Carousel } from "antd";
-import { LeftCircleOutlined } from "@ant-design/icons";
+import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons";
 import home1 from "../assets/home1.jpg";
 import home2 from "../assets/home2.jpg";
 import home3 from "../assets/home3.jpg";
@@ -11,14 +11,20 @@ import home4 from "../assets/home4.jpg";
 
 const Home = () => {
   const { Content, Footer } = Layout;
-
+  let carousel;
+  const onPrevClick = () => {
+    carousel.prev();
+  };
+  const onNextClick = () => {
+    carousel.next();
+  };
   return (
     <Layout className="layout">
       <Topbar selectedKey={"Home"} />
       <Content>
-        <Carousel className="carousel" autoplay>
+        <LeftCircleOutlined className="prev-btn" onClick={onPrevClick} />
+        <Carousel className="carousel" ref={(c) => (carousel = c)} autoplay>
           <div className="container">
-            {/* <LeftCircleOutlined className="prev-btn"/> */}
             <img src={home1} alt="first" />
           </div>
           <div className="container">
@@ -31,12 +37,19 @@ const Home = () => {
             <img src={home4} alt="fourth" />
           </div>
         </Carousel>
+        <RightCircleOutlined className="next-btn" onClick={onNextClick} />
         <div className="site-layout-content">
           <h1>Norton Historical Society</h1>
           <p>
-            Who we are: We are 501(c)(3) non-profit corporation dedicated to “preserving material relative to the history of Norton.”
+            Who we are: We are 501(c)(3) non-profit corporation dedicated to
+            “preserving material relative to the history of Norton.”
           </p>
-          <p className="bold" style={{display: "inline"}}>Recent Update: </p><p style={{display: "inline"}}>The Museum is current close due to COVID-19 pandemic</p>
+          <p className="bold" style={{ display: "inline" }}>
+            Recent Update:{" "}
+          </p>
+          <p style={{ display: "inline" }}>
+            The Museum is current close due to COVID-19 pandemic
+          </p>
           <p>Email or call to connect us</p>
           <p className="bold">nortonhistorical@gmail.com</p>
           <p className="bold">(508)285-8300</p>
